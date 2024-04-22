84d2f6315432a9d02e2454e380bbb5ffc893fabc
Gstechnology18 <125030405+Gstechnology18@users.noreply.github.com>
2024-04-22 11:50:58 -0300

    Versão 1.1

---
+++ login/src/main/java/com/example/login/model/Usuario.java
@@ -0,0 +1,19 @@
+// Usuario.java
+package com.example.login.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import jakarta.persistence.*;
+
+@Entity
+@Getter
+@Setter
+public class Usuario {
+ @Id
+ @GeneratedValue(strategy = GenerationType.IDENTITY)
+ private long cod;
+ private String username;
+ private String password;
+
+
+}
